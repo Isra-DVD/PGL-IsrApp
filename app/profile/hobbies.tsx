267518f447a72713f5e6c58abed61f2ed44bf9af
Isra-DVD <usr3849@salesianos-lacuesta.net>
2024-11-20 10:12:40 +0000

    refact(/profile/_layout): Refactorized the header display of the tab pages

---
+++ app/profile/hobbies.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Hobbies } from "../../components/Hobbies";
+
+const hobbies = () => {
+  return (
+    <>
+      <Hobbies />
+    </>
+  );
+};
+
+export default hobbies;
---
+++ app/profile/qr.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { QrCode } from "../../components/QrCode";
+
+const qr = () => {
+  return (
+    <>
+      <QrCode />
+    </>
+  );
+};
+
+export default qr;
