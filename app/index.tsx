34456d3aacbc8601d0467e7096b7c48584a27513
Isra-DVD <usr3849@salesianos-lacuesta.net>
2025-01-26 16:03:59 +0000

    feat(login-page)Implemented login-page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,20 @@
-import { Redirect } from "expo-router";
+import { router } from "expo-router";
+import { useEffect } from "react";
+import asyncStorageService from "../service/async-storage";
 
 const StartPage = () => {
-  return <Redirect href="/user-management/register-page" />;
+  useEffect(() => {
+    const hasToken = async () => {
+      const token = await asyncStorageService.getData("token");
+      if (token !== null) {
+        router.navigate("/home");
+      } else {
+        router.navigate("/user-management/login-page");
+      }
+    };
+
+    hasToken();
+  }, []);
 };
 
 export default StartPage;
---
+++ app/user-management/login-page.tsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { stylesLight } from "../../styles/styles";
+import apiService from "../../service/api-service";
+import { router } from "expo-router";
+
+const RegisterScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateInputs = () => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[cC][oO][mM]$/;
+
+    // ^[a-zA-Z0-9._%+-]+: La parte inicial del correo puede contener letras, números, puntos (.), guiones (-), o el signo más (+).
+    // @[a-zA-Z0-9.-]+: Debe incluir un @ seguido por un dominio que puede contener letras, números, puntos (.) o guiones (-).
+    // \.[cC][oO][mM]$: Asegura que el correo termine con .com (insensible a mayúsculas o minúsculas).
+
+    if (!emailRegex.test(email)) {
+      Alert.alert(
+        "Error",
+        "Por favor ingresa un email válido que contenga @ y termine en .com"
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const handleLogin = () => {
+    if (validateInputs()) {
+      apiService
+        .login(email, password)
+        .then((result) => {
+          Alert.alert("Info", result, [
+            {
+              text: "OK",
+              onPress: () => {
+                setEmail("");
+                setPassword("");
+                if (result !== "Pasaron cosas") {
+                  router.navigate("/home"); // A login
+                }
+              },
+            },
+          ]);
+        })
+        .catch((err) => {
+          Alert.alert("Error", "Cosas pasaron");
+        });
+    } else {
+      Alert.alert("Error", "Algo fue mal");
+    }
+  };
+
+  return (
+    <View style={stylesLight.registerContainer}>
+      <Text style={stylesLight.registerTitle}>Inicio de sesión</Text>
+
+      <TextInput
+        style={stylesLight.registerInput}
+        placeholder="Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        style={stylesLight.registerInput}
+        placeholder="Contraseña"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <TouchableOpacity
+        style={stylesLight.registerButton}
+        onPress={handleLogin}
+      >
+        <Text style={stylesLight.buttonText}>Iniciar sesión</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={stylesLight.registerButton}
+        onPress={() => router.navigate("/user-management/register-page")}
+      >
+        <Text style={stylesLight.buttonText}>Registrarse</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default RegisterScreen;
