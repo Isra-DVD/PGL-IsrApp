2fb6cd9f94910e3f26130e662ddf5f25ab7c7c4a
Isra-DVD <usr3849@salesianos-lacuesta.net>
2025-01-26 16:03:33 +0000

    feat(): Implemented asyncStorage for storing user's token

--- service/api-service.ts
+++ service/api-service.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import asyncStorageService from "./async-storage";
 
 const register = async (fullname: string, email: string, pswd: string) => {
   try {
@@ -16,8 +17,22 @@ const register = async (fullname: string, email: string, pswd: string) => {
   }
 };
 
+const login = async (email: string, pswd: string) => {
+  try {
+    const response = await axios.post("http://192.168.1.100:5000/auth/login", {
+      email,
+      pswd,
+    });
+    await asyncStorageService.storeData("token", response.data.object.token);
+    return response.data.message;
+  } catch (error) {
+    return "Pasaron cosas";
+  }
+};
+
 const apiService = {
   register,
+  login,
 };
 
 export default apiService;
