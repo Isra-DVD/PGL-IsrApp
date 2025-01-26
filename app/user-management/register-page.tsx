4224d023dc5734c9ceb255befb1255b88da82fce
Isra-DVD <usr3849@salesianos-lacuesta.net>
2025-01-26 15:03:00 +0000

    feat(): Implemented the API logic for the register-page

---
+++ app/user-management/register-page.tsx
@@ -0,0 +1,113 @@
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert } from "react-native";
+import { stylesLight } from "../../styles/styles";
+import apiService from "../../service/api-service";
+import { router } from "expo-router";
+
+const RegisterScreen = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateInputs = () => {
+    if (!fullName || fullName.length < 4) {
+      Alert.alert("Error", "El nombre debe tener al menos 4 caracteres.");
+      return false;
+    }
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[cC][oO][mM]$/;
+
+    // ^[a-zA-Z0-9._%+-]+: La parte inicial del correo puede contener letras, números, puntos (.), guiones (-), o el signo más (+).
+    // @[a-zA-Z0-9.-]+: Debe incluir un @ seguido por un dominio que puede contener letras, números, puntos (.) o guiones (-).
+    // \.[cC][oO][mM]$: Asegura que el correo termine con .com (insensible a mayúsculas o minúsculas).
+
+    if (!emailRegex.test(email)) {
+      Alert.alert(
+        "Error",
+        "Por favor ingresa un email válido que contenga @ y termine en .com"
+      );
+      return false;
+    }
+
+    const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/;
+
+    // ^(?=.*\d): Asegura que al menos un carácter sea un número (\d).
+    // (?=.*[a-zA-Z]): Asegura que haya al menos una letra (mayúscula o minúscula).
+    // .{8,}$: Asegura que la contraseña tenga al menos 8 caracteres en total.
+
+    if (!passwordRegex.test(password)) {
+      Alert.alert(
+        "Error",
+        "La contraseña debe tener al menos 8 caracteres y contener al menos un número."
+      );
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleRegister = () => {
+    if (validateInputs()) {
+      apiService
+        .register(fullName, email, password)
+        .then((result) => {
+          Alert.alert("Info", result, [
+            {
+              text: "OK",
+              onPress: () => {
+                setFullName("");
+                setEmail("");
+                setPassword("");
+                if (result !== "Pasaron cosas") {
+                  router.navigate("/home");
+                }
+              },
+            },
+          ]);
+        })
+        .catch((err) => {
+          Alert.alert("Error", "Cosas pasaron");
+        });
+    } else {
+      Alert.alert("Error", "Algo fue mal");
+    }
+  };
+
+  return (
+    <View style={stylesLight.registerContainer}>
+      <Text style={stylesLight.registerTitle}>Registro de Usuario</Text>
+
+      <TextInput
+        style={stylesLight.registerInput}
+        placeholder="Nombre completo"
+        value={fullName}
+        onChangeText={(text) => setFullName(text)}
+      />
+
+      <TextInput
+        style={stylesLight.registerInput}
+        placeholder="Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+
+      <TextInput
+        style={stylesLight.registerInput}
+        placeholder="Contraseña"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+
+      <TouchableOpacity
+        style={stylesLight.registerButton}
+        onPress={handleRegister}
+      >
+        <Text style={stylesLight.buttonText}>Registrarse</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default RegisterScreen;
---
+++ service/api-service.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { Alert } from "react-native";
+
+const register = async (fullname: string, email: string, pswd: string) => {
+  try {
+    const response = await axios.post(
+      "http://192.168.1.100:5000/auth/register",
+      {
+        fullname,
+        email,
+        pswd,
+      }
+    );
+    return response.data.message;
+  } catch (error) {
+    return "Pasaron cosas";
+  }
+};
+
+const apiService = {
+  register,
+};
+
+export default apiService;
